1a144430a61291f2f8cb08027dc20e9fdb0affae
saasbook <saasbook@saasbook.(none)>
2015-03-08 21:27:34 -0700

    Delete addfieldtoshift and add column to the create shift

--- er-core-staff-scheduler/db/migrate/20150309041911_create_shifts.rb
+++ er-core-staff-scheduler/db/migrate/20150309041911_create_shifts.rb
@@ -1,6 +1,11 @@
 class CreateShifts < ActiveRecord::Migration
   def change
     create_table :shifts do |t|
+    	t.string :starttime
+    	t.shiftslength :integer
+    	t.users :string[]
+    	t.owner :string
+    	t.open :boolean
 
       t.timestamps
     end
--- er-core-staff-scheduler/db/migrate/20150309042042_add_fields_to_shift.rb
+++
@@ -1,9 +0,0 @@
-class AddFieldsToShift < ActiveRecord::Migration
-  def change
-    add_column :shifts, :starttime, :string
-    add_column :shifts, :shiftlength, :integer
-    add_column :shifts, :users, :string[]
-    add_column :shifts, :owner, :string
-    add_column :shifts, :open, :boolean
-  end
-end
