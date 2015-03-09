01343ec26d0b567e1b3a257b3034e7168e2a5634
charlespark94 <charles.park94@berkeley.edu>
2015-03-09 01:08:32 -0300

    Edit naming convention

--- er-core-staff-scheduler/app/models/user.rb
+++ er-core-staff-scheduler/app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :usertype, :username, :password, :fte
 
-  def self.usertype
+  def self.all_usertype
   	['core', 'admin']
   end
 
-  def self.fte
+  def self.all_fte
   	[0.1, 0.2, 0.35, 0.4, 1.0]
   end
 end
