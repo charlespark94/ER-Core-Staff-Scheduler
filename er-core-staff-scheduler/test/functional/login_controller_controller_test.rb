58270de7a9b1435270647adb5075a44f60b5debe
saasbook <saasbook@saasbook.(none)>
2015-03-11 02:40:19 -0300

    login stuff

---
+++ er-core-staff-scheduler/test/functional/login_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LoginControllerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ er-core-staff-scheduler/test/unit/helpers/login_controller_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class LoginControllerHelperTest < ActionView::TestCase
+end
