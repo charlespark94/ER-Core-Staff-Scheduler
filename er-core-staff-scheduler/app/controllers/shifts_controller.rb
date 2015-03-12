27ff6bbbcd66c39a755bac9342d0aa47c05a4a7c
saasbook <saasbook@saasbook.(none)>
2015-03-11 20:28:15 -0700

    Working admin add

--- er-core-staff-scheduler/app/controllers/shifts_controller.rb
+++ er-core-staff-scheduler/app/controllers/shifts_controller.rb
@@ -1,12 +1,39 @@
-class ShiftsController < ApplicationController
-	
-	def index
-		@shifts = Shift.all
-	end
+class ShiftsController < ActionController::Base
+  def index
+    @shifts = Shift.all
+  end
 
-	def show
-		id = params[:id]
-		@shift = Shift.find(id)
-	end
+  def new
+    #@shift = Shift.new
+  end
 
+  def show
+    id = params[:id]
+    @shift = Shift.find(id)
+  end
+
+  def create
+    @shift = Shift.create!(params[:shift])
+    flash[:notice] = "Shift was successfully created."
+    redirect_to shifts_path
+  end
+
+  def edit
+
+    @shift = Shift.find params[:id]
+  end
+
+  def update
+    @shift = Shift.find params[:id]
+    @shift.update_attributes!(params[:shift])
+    flash[:notice] = "Shift was successfully updated."
+    redirect_to shift_path(@shift)
+  end
+
+  def destroy
+    @shift = Shift.find(params[:id])
+    @shift.destroy
+    flash[:notice] = "Shift deleted."
+    redirect_to shifts_path
+  end
 end
