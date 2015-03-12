2571228de64119ca2f8f41c67d9858dacaee544f
Jason Kim <jasonkim@berkeley.edu>
2015-03-12 02:11:59 -0300

    done with features?

--- er-core-staff-scheduler/app/controllers/availabilities_controller.rb
+++ er-core-staff-scheduler/app/controllers/availabilities_controller.rb
@@ -3,35 +3,18 @@ class AvailabilitiesController < ApplicationController
 	def index
 		@availabilities = Availability.all
 		@shifts = Shift.all
-		if params[:action]
 
-			# @shifts.each do |shift|
-			# 	if params.has_key?(1.to_s.to_sym)
-			# 		shift.update_attributes(:users => 2)
-			# 	end
-			# end
+		# @shifts.each do |shift|
+		# 	shift.update_attributes(:users => nil)
+		# 	shift.update_attributes(:possible_users => nil)
+		# end
 
-			@shifts.each do |shift|
-				shift.update_attributes(:users => shift.id)
+		@shifts.each do |shift|
+			if params[:"#{shift.id}"] == "yes"
+				shift.update_attributes(:users => 1)
+			elsif params[:"#{shift.id}"] == "maybe"
+				shift.update_attributes(:possible_users => 1)
 			end
-
-			# @shifts.each do |shift|
-			# 	if params[shift.id.to_s] == 'yes'
-			# 		shift.update_attributes(:users => 13) # this will be user.id down the line
-			# 	elsif params[shift.id.to_s] == 'maybe'
-			# 		shift.update_attributes(:possible_users => 2)
-			# 	end
-			# end
-
-			# params.each do |k, v|
-			# 	if v == 'yes'
-			# 		@shift = Shift.find(1)
-			# 		@shift.update_attributes(:users => 2)
-			# 	elsif v == 'maybe'
-			# 		@shift = Shift.find(2)
-			# 		@shift.update_attributes(:possible_users => 2)
-			# 	end
-			# end
 		end
 	end
 
