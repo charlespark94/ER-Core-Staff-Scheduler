ac31afbdd305bafa1f40c0db0166e230a6335bfc
saasbook <saasbook@saasbook.(none)>
2015-03-13 04:07:57 -0300

    rspec over 90%

--- er-core-staff-scheduler/app/controllers/shifts_controller.rb
+++ er-core-staff-scheduler/app/controllers/shifts_controller.rb
@@ -13,11 +13,7 @@ class ShiftsController < ActionController::Base
     flash[:notice] = "Shift was successfully created."
     redirect_to shifts_path
   end
-
-  def edit
-    @shift = Shift.find params[:id]
-  end
-
+  
   def update
     @shift = Shift.find params[:id]
     @shift.update_attributes!(params[:shift])
