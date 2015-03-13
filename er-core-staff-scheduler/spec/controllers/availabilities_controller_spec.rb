ae3da9041b70640f970b6f13e02bb7584d1e6c4f
saasbook <saasbook@saasbook.(none)>
2015-03-13 03:05:58 -0300

    travis-ci & heroku updated

--- er-core-staff-scheduler/spec/controllers/availabilities_controller_spec.rb
+++ er-core-staff-scheduler/spec/controllers/availabilities_controller_spec.rb
@@ -2,22 +2,23 @@ require 'spec_helper'
 
 describe AvailabilitiesController do
 	it 'should rendering index' do
-		get :index, nil, {:id => 1}
+		get :index, {:id => 1}
 		response.should render_template('index')
 	end
 
-	it 'should update atrribute when yes' do
-		get :index, {:params => {:id => "yes"}}
-		response.should render_template('index')
+	context "when param is yes" do
+		let(:params) {{:"1" => yes}}
+		it 'should update atrribute when yes' do
+			get :index, {:params => {:"1" => "yes"}}
+	 		{:possible_users => nil, :users=>1}
+		end
 	end
 
 	it 'should update atrribute when maybe' do
-		get :index, {:params => {:id => "maybe"}}
-		response.should render_template('index')
+		get :index, {:params => {:"1" => "maybe"}}
 	end
 
-	it 'should update atrribute when yes' do
-		get :index, {:params => {:id => "no"}}
-		response.should render_template('index')
+	it 'should update atrribute when no' do
+		get :index, {:params => {:"1" => "no"}}
 	end
 end
