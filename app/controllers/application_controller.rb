71c567233bbf5b16a5ca109408e9e65fb1369249
saasbook <saasbook@saasbook.(none)>
2015-03-20 22:20:29 -0300

    Page Cache Bug Fix: Can't press back key to go back to homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   helper_method :current_user
+  before_filter :destroy_page_cache
 
   def current_user
   	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def destroy_page_cache
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
   end
 end
