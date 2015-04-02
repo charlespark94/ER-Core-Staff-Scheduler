57878417ac431cb7292447faa46e01ae0a44ac7d
saasbook <saasbook@saasbook.(none)>
2015-04-02 00:38:46 -0700

    shifts

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,6 +1,25 @@
 class ShiftsController < ActionController::Base
+  include Calendar
+
   def index
     @shifts = Shift.all
+    @client = client
+    service = @client.discovered_api('calendar', 'v3')
+    event = {
+      'summary' => 'Appointment',
+      'location' => 'Somewhere',
+      'start' => {
+        'dateTime' => '2015-04-03T10:00:00.000-07:00'
+      },
+      'end' => {
+        'dateTime' => '2015-04-03T10:25:00.000-07:00'
+      }
+      }
+    result = client.execute(:api_method => service.events.insert,
+                        :parameters => {'calendarId' => 'primary'},
+                        :body => JSON.dump(event),
+                        :headers => {'Content-Type' => 'application/json'})
+    #@calendar = get_calendar
   end
 
   def new
