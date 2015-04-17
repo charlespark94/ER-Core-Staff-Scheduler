c6dfc12aa4d70963882be61c46a9ad29d626fdba
saasbook <saasbook@saasbook.(none)>
2015-04-17 11:03:55 -0300

    codeclimate

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -27,47 +27,28 @@ class AvailabilitiesController < ApplicationController
 				if @availability.nil?
 					@availability = shift.availabilities.build(:user_id => session[:user_id])
 				end
-				if params[:"#{shift.id}"] == "yes"
-					@availability.update_attributes(:availability => 2)
-				elsif params[:"#{shift.id}"] == "maybe"
-					@availability.update_attributes(:availability => 1)
-				elsif params[:"#{shift.id}"] == "no"
-					@availability.update_attributes(:availability => 0)
-				end
+				update_availability_helper(@availability, params[:"#{shift.id}"])
+				#if params[:"#{shift.id}"] == "yes"
+				#	@availability.update_attributes(:availability => 2)
+				#elsif params[:"#{shift.id}"] == "maybe"
+				#	@availability.update_attributes(:availability => 1)
+				#elsif params[:"#{shift.id}"] == "no"
+				#	@availability.update_attributes(:availability => 0)
+				#end
 				if !@availability.save
 					flash[:notice] = "Something bad happened, saving new ability in availabilities_controller"
 				end
 			end
+		end
+	end
 
-			# if users_exist?(shift.users)
-			# 	in_user = shift.users.split(" ").include?(session[:user_id])
-			# end
-			# if users_exist?(shift.possible_users)
-			# 	in_pos = shift.possible_users.split(" ").include?(session[:user_id].to_s)
-			# end 
-			# 	#not_choose = (not in_pos) and (not in_user)
-			# if (in_user || in_pos)
-			# 	not_choose = false
-			# else
-			# 	not_choose = true
-			# end
-			# if params[:"#{shift.id}"] == "yes"
-				
-			# 	if not_choose
-			# 		newstring = "#{shift.users} #{session[:user_id]}"
-			# 		shift.update_attributes(:users => newstring)
-			# 	end
-			# 	#shift.update_attributes(:possible_users => nil)
-			# elsif params[:"#{shift.id}"] == "maybe"
-			# 	if not_choose
-			# 		newstring = "#{shift.possible_users} #{session[:user_id]}"
-			# 		shift.update_attributes(:possible_users => newstring)
-			# 	end
-			# 	#shift.update_attributes(:users => nil)
-			# elsif params[:"#{shift.id}"] == "no"
-			# 	#shift.update_attributes(:possible_users => nil)
-			# 	#shift.update_attributes(:users => nil)
-			# end
+	def update_availability_helper(availability, val)
+		if val == "yes"
+			availability.update_attributes(:availability => 2)
+		elsif val == "maybe"
+			availability.update_attributes(:availability => 1)
+		elsif val == "no"
+			availability.update_attributes(:availability => 0)
 		end
 	end
 
