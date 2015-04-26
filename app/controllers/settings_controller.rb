70495fbc7d4fdedf46f21867bb4418c687f33437
saasbook <saasbook@saasbook.(none)>
2015-04-25 22:05:25 -0700

    Settings fix

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -10,11 +10,15 @@ class SettingsController < ApplicationController
 
   def update
     @user = User.find_by_id(session[:user_id])
-    if params[:password].nil?
+    if params[:user][:password] == ""
     	params[:password] = @user.password
     	params[:password_confirmation] = @user.password_confirmation
+    elsif params[:user][:password] != params[:user][:password_confirmation]
+    	flash[:notice] = "Password and Password Confirmation must match"
+    	redirect_to edit_setting_path(@user) and return
     end
     @user.update_attributes(params[:user])
+    flash[:notice] = "Your information has been updated"
     redirect_to settings_path
   end
 end
