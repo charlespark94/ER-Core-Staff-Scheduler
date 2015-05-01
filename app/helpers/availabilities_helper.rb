0125fae711fb34a438e328c2ecb81c326d5df85f
saasbook <saasbook@saasbook.(none)>
2015-05-01 02:30:51 -0700

    heroku fix

--- app/helpers/availabilities_helper.rb
+++ app/helpers/availabilities_helper.rb
@@ -5,9 +5,10 @@ module AvailabilitiesHelper
 
 	def recur_helper(shift)
 		prev_shift = Shift.where("shiftstart = ?", shift.shiftstart - 2.weeks).where("shiftend = ?", shift.shiftend - 2.weeks)
-		return if prev_shift.nil?
-		availability = Availability.where(user_id: session[:user_id], shift_id: shift.id).first
-		prev_avail = Availability.where(user_id: session[:user_id], shift_id: prev_shift[0].id).first
-		availability.update_attribute(:availability, prev_avail.availability)
+		if !prev_shift.nil?
+			availability = Availability.where(user_id: session[:user_id], shift_id: shift.id).first
+			prev_avail = Availability.where(user_id: session[:user_id], shift_id: prev_shift[0].id).first
+			availability.update_attribute(:availability, prev_avail.availability)
+		end
 	end
 end
