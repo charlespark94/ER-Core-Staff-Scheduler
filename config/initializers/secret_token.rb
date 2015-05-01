54f185b3c45b8f897fb585c53f828fc2d0051b3c
Jason Kim <jasonkim@berkeley.edu>
2015-05-01 17:04:10 -0300

    secret key gone

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# ErCoreStaffScheduler::Application.config.secret_token = 'fc1f33c3a4a1aa36ba74b6b37b42b3eb84a2551b07fcbe1cf1577b43314222b4c59ba98b8f898bb26e8281db49a960cfcb30f13457d427b334e1783fa46aba76'
 ErCoreStaffScheduler::Application.config.secret_token = ENV['SECRET_TOKEN']
