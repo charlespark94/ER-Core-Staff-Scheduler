580465da003e7aa01dc5e32fa860ca6b43ea382c
saasbook <saasbook@saasbook.(none)>
2015-05-01 02:37:54 -0700

    Changes

--- app/helpers/availabilities_helper.rb
+++ app/helpers/availabilities_helper.rb
@@ -5,8 +5,7 @@ module AvailabilitiesHelper
 
 	def recur_helper(shift)
 		prev_shift = Shift.where("shiftstart = ?", shift.shiftstart - 2.weeks).where("shiftend = ?", shift.shiftend - 2.weeks)
-		raise "#{prev_shift}            #{prev_shift[0]}".inspect
-		return if prev_shift.nil?
+		return if prev_shift[0].nil?
 		availability = Availability.where(user_id: session[:user_id], shift_id: shift.id).first
 		prev_avail = Availability.where(user_id: session[:user_id], shift_id: prev_shift[0].id).first
 		availability.update_attribute(:availability, prev_avail.availability)
