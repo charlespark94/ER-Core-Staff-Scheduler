a669f0160f19c9c4c590658eabe9482ace150a8d
saasbook <saasbook@saasbook.(none)>
2015-05-01 01:54:27 -0700

    smell fix

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -32,16 +32,18 @@ module NavigationHelpers
     when /^settings$/
       edit_setting_path(User.find_by_id(1))
     else
-      begin
-        page_name =~ /^the (.*) page$/
-        path_components = $1.split(/\s+/)
-        self.send(path_components.push('path').join('_').to_sym)
-      rescue NoMethodError, ArgumentError
-        raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
-          "Now, go and add a mapping in #{__FILE__}"
-      end
+      helper
     end
   end
 end
 
+def helper
+  begin
+    page_name =~ /^the (.*) page$/
+    path_components = $1.split(/\s+/)
+    self.send(path_components.push('path').join('_').to_sym)
+  rescue NoMethodError, ArgumentError
+    raise "Can't find mapping from \"#{page_name}\" to a path.\n" + "Now, go and add a mapping in #{__FILE__}"
+  end
+end
 World(NavigationHelpers)
