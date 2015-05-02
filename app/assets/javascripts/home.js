d070f2db68759b5cde59f439bdef8eecf9607731
David Pei <davidapei@gmail.com>
2015-05-01 18:37:49 -0700

    added sidebar collapser/expander

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,23 @@
+function expandSidebar() {
+	$('#sidebar-wrapper').css('width', '250px')
+	$('#wrapper').css('padding-left', '250px')
+	$('#expander').css('display', 'none')
+}
+
+function shrinkSidebar() {
+	$('#sidebar-wrapper').css('width', '0px')
+	$('#wrapper').css('padding-left', '0px')
+	$('#expander').css('display', 'block')
+}
+
+function expandOrShrink() {
+	if ($('#wrapper').css('padding-left') == '0px') {
+		$('#sidebar-wrapper').css('width', '250px')
+		$('#wrapper').css('padding-left', '250px')
+		$('#sidebar-show').html('<<<')
+	} else {
+		$('#sidebar-wrapper').css('width', '0px')
+		$('#wrapper').css('padding-left', '0px')
+		$('#sidebar-show').html('>>>')
+	}
+}
